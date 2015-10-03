001c975786f088be2a04b6a0de7c0025d75dc0b8
mishuk222 <mishuk1971@gmail.com>
2015-10-03 22:26:04 +0600

    Added the file

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpAssignment
+{
+    class ID
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name: ");
+            Console.WriteLine("ID: ");
+            Console.ReadKey();
+        
+        
+        
+        }
+    
+    
+    
+    }
+}
